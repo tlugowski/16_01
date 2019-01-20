ddc4c92f837bab146c8b5d95c2a0e71c7714452d
Tomasz <Lugowski>
2019-01-20 18:59:32 +0100

    Changing Title and Price name to Tytul i cena

--- 16_01/Models/Book.cs
+++ 16_01/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,10 @@ namespace _16_01.Models
 {
     public class Book
     {
+        [Display(Name = "Tytuł")]
         public string Title { get; set; }
+
+        [Display(Name = "Cena")]
         public int Price { get; set; }
     }
 }
