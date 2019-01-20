1fbf9e185c476dcd1d0d09a3b7d33b845e4ff232
Tomasz <Lugowski>
2019-01-20 19:41:40 +0100

    Add registration class to Models

---
+++ 16_01/Models/Registration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _16_01.Models
+{
+    public class Registration
+    {
+        [Display(Name = "Imię")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Adres")]
+        public string Address { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
+        public string PostCode { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telefon")]
+        public int Telephone { get; set; }
+    }
+}
